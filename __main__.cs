d1b3f4e755653e6d3d9e9b718888ff2d7591b05a
Your Name <noludex@mail.ru>
2023-04-24 21:35:42 +0300

    Шаблон получение данных с БД организации

---
+++ __main__.cs
@@ -0,0 +1,10 @@
+namespace VeterinaryClinicRB
+{
+    class __main__
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ class/clientele.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeterinaryClinicRB
+{
+    public class organization
+    {
+        // Получение данных из БД и запись как объектов
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+    }
+}
