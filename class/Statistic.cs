466745c3e03b2689ba9d33ee38a82ca773fd12b4
Emowantt <iliavaraksa2006@gmail.com>
2023-04-26 20:57:49 +0300

    Спасибо сыну за пробелы

---
+++ class/Statistic.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeterinaryClinicRB
+{
+    public class Statistic
+    {
+        public string date {get; set;}
+        public string description {get; set;}
+
+        // Конструктор класса
+        public Statistic(string date, string description)
+        {
+            // Присвоение значений
+            this.date = date;
+            this.description = description;
+        }
+        public Statistic()
+        {
+            // Присвоение значений, если их нет
+            this.
+        }
+    } 
+       
+}
