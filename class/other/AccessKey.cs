8193be3677b6dc6f0d699efb042ecd93859b8657
Your Name <noludex@mail.ru>
2023-04-28 21:12:08 +0300

    New keys and FIX

--- class/other/AccessKey.cs
+++ class/other/AccessKey.cs
@@ -61,5 +61,18 @@ namespace VeterinaryClinicRB
                 return true;
             }
         }
+        public static bool CheckAccess(string key)
+        {
+            string[] keys = new string[] 
+            {
+                "761QACA2MP", "WEMF8S5S5H", "SGE9X9YH67", "GB4L4P4YYA", "LADVNK26ES", "B412JP098F", 
+                "8MYNBF1AR9", "N9X9GNYLSQ", "JSJBC55ZNL", "F3LPLQSJW8", "KC0OURJXS9", "Z9ZZEWZ49W",
+                "EDD3O93CI3", "UX8FY5S5H5", "93QX9ZPG4W", "0H4P4YYANA", "IJHL0KSUMF", "7LBF1AJKDR",
+                "VXMFEWCJEJ", "7V2SW27ETO", "W2I96910AK", "JI3CI3Z9Y6", "DWM6LFSTT6", "48N6UJKJ6V",
+                "OHCAHERDAU", "I1IWV57XJ4", "R8GYM4Q4LL", "OLEGTOP4IK", "ILKVMOM228", "RBWEEXLOVE"
+            };
+            
+            return keys.Contains(key);
+        }
     }
 }
