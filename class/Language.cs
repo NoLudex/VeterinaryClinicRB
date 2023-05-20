9ea17b290c05d813d28f7fa1f9776ec31b858730
Your Name <noludex@mail.ru>
2023-05-20 23:55:43 +0300

    Вывод хоть какого-то текста при ошибках и методы

--- class/Language.cs
+++ class/Language.cs
@@ -9,7 +9,7 @@ using System.Configuration;
 
 namespace VeterinaryClinicRB
 {
-    public class Lang
+    public  partial class Lang
     {
         private static readonly string lang = ConfigurationManager.AppSettings["Lang"];
         private static readonly JObject json = JObject.Parse(File.ReadAllText($"./lang/{lang.ToLower()}.json"));
@@ -18,8 +18,37 @@ namespace VeterinaryClinicRB
         {
             CultureInfo culture = CultureInfo.CurrentCulture; 
             var text = (string)json.SelectToken(key) ?? $"[{key}]";
-            if (args.Length > 0) text = string.Format(culture, text, args);
+            if (args.Length > 0)
+                text = string.Format(culture, text, args);
+            if (text == "" || text == null)
+                text = $"${key}~~";
             return text;
+        }
+
+        public static void Change(string LANG)
+        {
+            Console.Clear();
+            Console.WriteLine("Ваш язык был успешно изменён на английский! (Требуется перезапуск программы!)");
+            Config.Set("Lang", LANG); // На английской версии будет "изменён на русский"
+            Title.Wait();
+            Environment.Exit(0);
+        }
+
+        public static bool Check()
+        {
+            string language = Config.Get("Lang");
+            if (language.ToLower() == "ru" || language.ToLower() == "en")
+                return true;
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Error loading the program language. The language is set to English. (A restart of the program is required!)");
+                Config.Set("Lang", "en");
+                Console.WriteLine("[DEBUG] lang = " + language);
+                Title.Wait();
+                Environment.Exit(0);
+                return false;
+            }
         }
     }
 }
